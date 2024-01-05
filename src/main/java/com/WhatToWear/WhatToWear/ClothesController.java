2ccfc8ecc4267dab773e091bde9ea3322a895c87
Martin Paul Thon <martin-thon@gmx.de>
2024-01-05 15:10:52 +0100

    CORS functioning

--- src/main/java/com/WhatToWear/WhatToWear/ClothesController.java
+++ src/main/java/com/WhatToWear/WhatToWear/ClothesController.java
@@ -3,9 +3,11 @@ package com.WhatToWear.WhatToWear;
 import com.WhatToWear.WhatToWear.Model.Clothing;
 import com.WhatToWear.WhatToWear.repo.ClothesRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class ClothesController {
